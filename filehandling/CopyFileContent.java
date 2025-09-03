996fc92c793aa1e8aa553efecd67a1bf2d0dc4c3
Honey R Shetty <honeyshetty2003@gmail.com>
2025-09-03 11:21:23 +0530

    copy_file_content

---
+++ filehandling/CopyFileContent.java
@@ -0,0 +1,24 @@
+package filehandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyFileContent {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String sourceFile = "D:\\honey\\source.txt";
+		String destinationFile = "D:\\honey\\destination.txt";
+
+		try (FileReader reader = new FileReader(sourceFile);FileWriter writer = new FileWriter(destinationFile)) {
+				int data = reader.read();
+				while (data != -1) {
+					writer.write((char) data);
+					data = reader.read();
+				}
+				 System.out.println("File copied successfully!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
